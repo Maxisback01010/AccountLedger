f4bf6424e63f6be41e4628e1ca602951077e84ea
Max <marleyjodici@gmail.com>
2026-04-29 10:51:41 -0700

    Updated String

---
+++ src/main/java/Transaction.java
@@ -0,0 +1,19 @@
+public class Transaction {
+
+    String date;
+    String time;
+    String description;
+    String vendor;
+    double amount;
+    public Transaction (String date, String time, String description, String vendor,double amount){
+        this.date=date;
+        this.time=time;
+        this.description=description;
+        this.vendor=vendor;
+        this.amount=amount;
+
+
+
+
+    }
+}
