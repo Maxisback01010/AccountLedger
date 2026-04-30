1bbb4c48c0aeae96e03df41cc1b76b9f4bbe8839
Max <marleyjodici@gmail.com>
2026-04-30 12:56:16 -0700

    Updated Variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("App started!");
-    }
-}
 
+        Transaction t = new Transaction(
+                "2026-04-30",
+                "15:45:00",
+                "Software",
+                "Gamestop",
+                -82.75
+
+        );
+        System.out.println(t);
+    }
+
+}
