9ad8c19ef6e1bc59e22e344ccf3628264922210a
Max <marleyjodici@gmail.com>
2026-05-01 07:56:52 -0700

    Fixed project structure - moved files to src/main/java

---
+++ src/main/resources/Transaction.java
@@ -0,0 +1,21 @@
+public class Transaction{
+
+    String date;
+    String time;
+    String description;
+    String vendor;
+    double amount;
+    public Transaction (String date, String time, String description, String vendor,double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+        public String toString() {
+            return date + "|" + time + "|"+ description + "|" + vendor + "|" + amount;
+        }
+
+
+}
